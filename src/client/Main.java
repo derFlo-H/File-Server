6cdc862690cf1f21b383b22bf408004302805caa
flo <florianhagemann@gmx.net>
2021-10-22 22:07:10 +0200

    added basic socket stuff for client

--- src/client/Main.java
+++ src/client/Main.java
@@ -3,10 +3,29 @@ package client;
 import java.io.*;
 import java.net.*;
 
+/*
+ * Client for file server
+ */
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		try {
+			
+			// Create Socket and Data streams
+			Socket s = new Socket("localhost", 69);
+			DataInputStream dIn = new DataInputStream(s.getInputStream());
+			DataOutputStream dOut = new DataOutputStream(s.getOutputStream());
+			
+			// Close data streams and socket
+			dOut.close();
+			dIn.close();
+			s.close();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 
 	}
 
